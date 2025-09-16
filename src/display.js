47dacb0fafac5a736f30342c9fd58ca78b1ac02e
Sakolkiat <sakolkiat.n@gmail.com>
2025-09-17 03:25:29 +0700

    Add more displayData

--- src/display.js
+++ src/display.js
@@ -1,7 +1,7 @@
 // input address
-// get api data
+// get api data/
 // loading animation
-// display data on screen
+// display data on screen/
 // 
 // if search again
 // remove content
@@ -21,7 +21,6 @@ export function test() {
 	activeSearchBtn();
 }
 
-
 function displayData(id, data, symbol) {
 	const target = document.getElementById(id);
 	if (symbol == undefined) symbol = '';
@@ -31,44 +30,25 @@ function displayData(id, data, symbol) {
 
 async function updateDisplay() {
 	const data = await getWeatherData();
-	// address: data.address,
-	// temp: condition.temp,
 	// feelslike: condition.feelslike,
-	// humidity: condition.humidity,
-	// precipprob: condition.precipprob,
-	// windspeed: condition.windspeed,
-	// uvindex: condition.uvindex,
-	// conditions: condition.conditions,
 	// icon: condition.icon,
-	// sunrise: condition.sunrise,
-	// sunset: condition.sunset,
 	// forecast: forecastInfo(data),
 
-	// <p id="today-temp">999°C</p>
-	// <p id="today-date">today...</p>
-	// <p id="today-condition">rain</p>
-	// <p id="rain-prob">% rain</p>
-	// <p id="address">address</p>
 	displayData('today-temp', data.temp, ' °C');
 	displayData('address', data.address);
 	displayData('today-condition', data.conditions);
 	displayData('rain-prob', data.precipprob, '%');
+	displayData('hl-wind', data.windspeed);
+	displayData('hl-hum', data.humidity);
+	displayData('hl-uv', data.uvindex);
+	displayData('hl-sunri', data.sunrise);
+	displayData('hl-sunse', data.sunset);
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// pick data -> data.forecast[i].target
+// date: day[i].datetime,
+// condition: day[i].conditions,
+// icon: day[i].icon,
+// tempmax: fahToCel(day[i].tempmax),
+// tempmin: fahToCel(day[i].tempmin),
